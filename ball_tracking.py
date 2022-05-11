33af229673241b0f8018f7a1cec5236201a4cd75
erripe13 <pierre.mrz13@gmail.com>
2022-05-11 10:45:08 +0200

    Update ball_tracking.py

--- ball_tracking.py
+++ ball_tracking.py
@@ -16,32 +16,26 @@ time.sleep(2.0)
 
 # main
 while True:
-	# grab the current frame
+	# def frame comme flux video
 	frame = vs.read()
-	# handle the frame from VideoCapture or VideoStream
 	frame = frame[1] if args.get("video", False) else frame
-	# if we are viewing a video and we did not grab a frame,
-	# then we have reached the end of the video
+	# fin de la boucle si pas de flux
 	if frame is None:
 		break
-	# resize the frame, blur it, and convert it to the HSV
-	# color space
+	# réduction de la résolution, flou, et conversion HSV
 	frame = imutils.resize(frame, width=600)
 	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
+	# génération d'un masque sur la balle
 	mask = cv2.inRange(hsv, greenLower, greenUpper)
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2)
-	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
+	# trouver contours de la sortie du filtre et coordonnées de son centre
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
 	cnts = imutils.grab_contours(cnts)
 	center = None
-	# only proceed if at least one contour was found
+	# si un contour est trouvé :
 	if len(cnts) > 0:
 		# find the largest contour in the mask, then use
 		# it to compute the minimum enclosing circle and
