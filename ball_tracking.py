d8af9e761fce595a80ff4a04f0eb3e42b94d3f99
erripe13 <pierre.mrz13@gmail.com>
2022-05-11 14:26:24 +0200

    +cao

--- ball_tracking.py
+++ ball_tracking.py
@@ -12,7 +12,7 @@ import time
 # limites HSV couleur balle, à définir avec hsv_define.py
 greenLower = (0, 106, 82)
 greenUpper = (159, 231, 225)
-pts = deque(maxlen=args["buffer"])
+pts = deque(maxlen=50)
 # attribution flux webcam
 vs = VideoStream(src=0).start()
 # attente démarrage cam
@@ -22,7 +22,7 @@ time.sleep(2.0)
 while True:
 	# définir frame comme flux video
 	frame = vs.read()
-	frame = frame[1] if args.get("video", False) else frame
+	#frame = frame[1] if args.get("video", False) else frame
 	# fin de la boucle si pas de flux
 	if frame is None:
 		break
@@ -59,10 +59,10 @@ while True:
 		if pts[i - 1] is None or pts[i] is None:
 			continue
 		# calculer l'épaisseur de la ligne en fonction du temps et dessiner
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		thickness = int(np.sqrt(10 / float(i + 1)) * 2.5)
 		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 	# afficher la sortie video traitée assemblée
-	cv2.putText(image,"test",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
+	cv2.putText(frame,"x,y: "+str(x)+","+str(y),(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 	# arrêt si Q est pressé
