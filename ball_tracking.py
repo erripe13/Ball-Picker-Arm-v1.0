42dbc0efbca958845f90f04423619e5e86afda21
erripe13 <pierre.mrz13@gmail.com>
2022-05-11 10:42:30 +0200

    update

--- ball_tracking.py
+++ ball_tracking.py
@@ -2,33 +2,19 @@
 from collections import deque
 from imutils.video import VideoStream
 import numpy as np
-import argparse
 import cv2
 import imutils
 import time
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-	help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-	help="max buffer size")
-args = vars(ap.parse_args())
-# define the lower and upper boundaries of the "orange"
-# ball in the HSV color space, then initialize the
-# list of tracked points
+# limites HSV couleur balle, à définir avec hsv_define.py
 greenLower = (0, 106, 82)
 greenUpper = (159, 231, 225)
 pts = deque(maxlen=args["buffer"])
-# if a video path was not supplied, grab the reference
-# to the webcam
-if not args.get("video", False):
-	vs = VideoStream(src=0).start()
-# otherwise, grab a reference to the video file
-else:
-	vs = cv2.VideoCapture(args["video"])
-# allow the camera or video file to warm up
+# attribution flux webcam
+vs = VideoStream(src=0).start()
+# attente démarrage cam
 time.sleep(2.0)
-# keep looping
+
+# main
 while True:
 	# grab the current frame
 	frame = vs.read()
