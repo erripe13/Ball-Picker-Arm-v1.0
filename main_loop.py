addc8035e3ddd769cacc2ee7d49f63cc3faa292d
erripe13 <pierre.mrz13@gmail.com>
2022-05-16 11:21:13 +0200

    Update main_loop.py

--- main_loop.py
+++ main_loop.py
@@ -19,33 +19,30 @@ class main_loop:
 
     def __init__(self, serialport='/dev/ttyACM1',x_arm_adj=0,y_arm_adj=0):
 
-        #Calibrate Camera XY vs. Arm coordinates XY
-        #x_camera=x_arm    y_camera=y_arm
-        self.x_arm_adj=2.62 #cam_x Zero Position vs. arm X zero position
-        self.y_arm_adj=8.85 #cam_y Zero Position vs. arm Y zero positoin
-
-        #initialize detection
+        #calibration XY cam par rapport au XY du bras
+        self.x_arm_adj=2.62 #origine X offset
+        self.y_arm_adj=8.85 #origine Y offset
         self.cameraXYZ=camera_realworldxyz.camera_realtimeXYZ()
 
-        #initiate arduino
+        #initialisation arduino
         #serialport='/dev/ttyACM1',LOW_Z=-20.5, HOVER_Z=-16.5,DROP_Z=-18.5, MID_Z=-15, HIGH_Z=-6
         self.arm_c=commands_arduino.arm_controller(serialport,-20.5,-16.5,-18.5,-15,-6)
 
 
     def truncate(self, n, decimals=0):
-        #this function will help round numbers to prepare them to pass into arduino
+        #formattage des nombres pour communication série
         n=float(n)
         multiplier = 10 ** decimals
         return int(n * multiplier) / multiplier
 
     def capturefromPiCamera(self, imgdir,prefix="Cap",fullscreen=False,detectXYZ=True, calcXYZ=True, arm=True):
 
-            #initiate counters
+            #init compteurs
             img_counter=0
             bg_counter=0
             id_counter=0
             
-            #initiate flags
+            #init des flags
             bg_capture=False
             move_object=False
             arm_move=False
@@ -54,11 +51,11 @@ class main_loop:
             obj_detected=0
             obj_detected_prev=0
 
-            #move arm to clear the camera vision
+            #mouvement pour libérer la vue de la caméra
             if (arm==True): self.arm_c.move_clearcamera()
 
 
-            #initiate camera and window
+            #initialisation caméra et fenêtre display
             camera = PiCamera()
             camera.resolution = (1280, 720)
             camera.framerate = 20
@@ -73,10 +70,10 @@ class main_loop:
 
 
 
-            # capture frames from the camera
+            # capture du flux caméra
             for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
-                    #listen for keystrokes
+                    #check si interruption clavier
                     k = cv2.waitKey(1)
                     
                     image = frame.array
@@ -84,45 +81,44 @@ class main_loop:
                     #image=cv2.flip(image_cap, -1 )
 
                     # capture background
-                    if bg_capture==False:
-                            bg_counter+=1
-                            #letting counter reach 52 provides warm up for camera, then capture background
-                            if bg_counter==52:
-                                self.cameraXYZ.load_background(image)
-                                bg_capture=True
-                                print("background captured")
-                            else:
-                                cv2.putText(image,"Warming Up",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
-
-                    # with background captured, image detection will now work
-                    if bg_capture==True:
-                            #for the camera to detect the same number of objects for 20 frames, before triggering arm
-                            if id_counter>20:
-                                    cv2.putText(image,"Picking",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2)
-                                    print("Trigger Arm")
-                                    #arm flag ensures movement can happen
-                                    self.pickanddrop(XYZ,arm)
-                                    id_counter=0
-                            #run detection when on
-                            if detectXYZ==True:
-                                    obj_detected_prev=obj_detected
-                                    image, XYZ = self.cameraXYZ.detect_xyz(image,calcXYZ)
-                                    obj_detected=len(XYZ)
-                                    #start counter when same number of objects detected between frames
-                                    if obj_detected>0 and obj_detected==obj_detected_prev:
-                                            id_counter+=1
-                                            cv2.putText(image,"Object Detected",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),2)
+                    # if bg_capture==False:
+                    #         bg_counter+=1
+                    #         #letting counter reach 52 provides warm up for camera, then capture background
+                    #         if bg_counter==52:
+                    #             self.cameraXYZ.load_background(image)
+                    #             bg_capture=True
+                    #             print("background captured")
+                    #         else:
+                    #             cv2.putText(image,"Warming Up",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
+                    # # with background captured, image detection will now work
+                    # if bg_capture==True:
+                    #         #for the camera to detect the same number of objects for 20 frames, before triggering arm
+                    #         if id_counter>20:
+                    #                 cv2.putText(image,"Picking",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2)
+                    #                 print("Trigger Arm")
+                    #                 #arm flag ensures movement can happen
+                    #                 self.pickanddrop(XYZ,arm)
+                    #                 id_counter=0
+                    #         #run detection when on
+                    #         if detectXYZ==True:
+                    #                 obj_detected_prev=obj_detected
+                    #                 image, XYZ = self.cameraXYZ.detect_xyz(image,calcXYZ)
+                    #                 obj_detected=len(XYZ)
+                    #                 #start counter when same number of objects detected between frames
+                    #                 if obj_detected>0 and obj_detected==obj_detected_prev:
+                    #                         id_counter+=1
+                    #                         cv2.putText(image,"Object Detected",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),2)
                          
                     if k%256 == 27:
                             # ESC pressed
                             print("Exiting...")
                             break
-                    elif k%256 == 32:
-                            # SPACE pressed Captures an Image
-                            img_name = imgdir + prefix + "_{}.jpg".format(img_counter)
-                            cv2.imwrite(img_name, image)
-                            print(prefix + "_{} written!".format(img_counter))
-                            img_counter += 1
+                    # elif k%256 == 32:
+                    #         # SPACE pressed Captures an Image
+                    #         img_name = imgdir + prefix + "_{}.jpg".format(img_counter)
+                    #         cv2.imwrite(img_name, image)
+                    #         print(prefix + "_{} written!".format(img_counter))
+                    #         img_counter += 1
 
 
                     # show the frame
