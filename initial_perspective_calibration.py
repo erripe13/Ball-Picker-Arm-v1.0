83694eb36fa6a40b8ae9efe6146477a1ac4c8b0d
erripe13 <pierre.mrz13@gmail.com>
2022-05-25 10:58:20 +0200

    Update initial_perspective_calibration.py

--- initial_perspective_calibration.py
+++ initial_perspective_calibration.py
@@ -55,7 +55,7 @@ worldPoints=np.array([[X_center,Y_center,Z_center],
                        [463,315,710],
                        [150,510,777],
                        [307,510,773],
-                       [463,510,805], dtype=np.float32)
+                       [463,510,805]], dtype=np.float32)
 
 #MANUALLY INPUT THE DETECTED IMAGE COORDINATES HERE
 
@@ -69,7 +69,7 @@ imagePoints=np.array([[cx,cy],
                        [183,203],
                        [388,767],
                        [288,85],
-                       [192,90], dtype=np.float32)
+                       [192,90]], dtype=np.float32)
 
 
 #FOR REAL WORLD POINTS, CALCULATE Z from d*
