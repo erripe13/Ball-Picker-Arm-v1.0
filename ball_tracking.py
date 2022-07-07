b8514853da5510c4fc0f5a4aa968c51382b27f8a
erripe13 <pierre.mrz13@gmail.com>
2022-07-07 11:22:48 +0200

    update

--- ball_tracking.py
+++ ball_tracking.py
@@ -58,86 +58,103 @@ def catchball(x, y):
 						arduino.write(inputs)
 						if answer== 'done' :
 							break
-					
+
+
 # main
 while True:
-	
-	# définir frame comme flux video
-	frame = vs.read()
-	#frame = frame[1] if args.get("video", False) else frame
-	# fin de la boucle si pas de flux
-	if frame is None:
-		break
-	# réduction de la résolution, flou, et conversion HSV
-	frame = frame[20:450, 60:600]
-	calib = cv2.circle(frame,(84,38), 5,(0,0,255),-1)
-	calib = cv2.circle(calib,(449,36),5,(0,0,255),-1)
-	calib = cv2.circle(calib,(34,419),5,(0,0,255),-1)
-	calib = cv2.circle(calib,(517,415),5,(0,0,255),-1) 
-	imgPts = np.float32([[84,38],[449,36],[34,419],[517,415]])
-	objPoints = np.float32([[0,0],[1000,0],[0,1000],[1000,1000]])
-	matrix = cv2.getPerspectiveTransform(imgPts,objPoints)
-	frame = cv2.warpPerspective(frame,matrix,(1000,1000))
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-	# génération d'un masque sur la balle
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
-	# trouver contours de la sortie du filtre et coordonnées de son centre
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	center = None
-	# si un contour est trouvé :
-	if len(cnts) > 0:
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-		# préciser le rayon minimal pour la détection (en pixels)
-		if radius > 10:
-			# dessiner le cercle et mettre à jour la liste des dernières coordonnées (pour le trail)
-			cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 0), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)			
-			#conversion en centimètres
-			x=((x/1000)*64)+1
-			y=((y/1000)*64)-0.4
-			x=round(x,1)
-			y=round(y,1)
-			cv2.putText(frame,"x,y: "+str(x)+","+str(y),(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2)
-			if (xpast>=x-0.25 and xpast<=x+0.25 and ypast>=y-0.25 and ypast<=y+0.25):
-				cv2.putText(frame,"STOP",(20,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-				counter=counter+1
-				if (counter>=15) :
-					cv2.putText(frame,"SEND",(20,90),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2)
-					catchball(x, y)
-					counter=0
-			xpast=x
-			ypast=y
-	# màj des données
-	pts.appendleft(center)
-	# fonction pour le trail rouge
-	for i in range(1, len(pts)):
-		# si pas de données, sauter
-		if pts[i - 1] is None or pts[i] is None:
-			continue
-		# calculer l'épaisseur de la ligne en fonction du temps et dessiner
-		thickness = int(np.sqrt(10 / float(i + 1)) * 2.5)
-		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-		# afficher la sortie video traitée assemblé
-	if GPIO.input(buttonPin) == GPIO.HIGH:
-		#cv2.namedWindow("pré-calib", cv2.WND_PROP_FULLSCREEN)	
-		#cv2.setWindowProperty("pré-calib",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-		cv2.imshow("pré-calib", calib)
-	elif GPIO.input(buttonPin) == GPIO.LOW:
-		#cv2.namedWindow("Retour tracking", cv2.WND_PROP_FULLSCREEN)	
-		#cv2.setWindowProperty("Retour tracking",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-		cv2.imshow("Retour tracking", frame)
-	key = cv2.waitKey(1) & 0xFF
-	# arrêt si Q est pressé
-	if key == ord("q"):
-		break
-# arrêt de la camera
-vs.stop()
-# fermer les fenetres
-cv2.destroyAllWindows()
+	with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+		time.sleep(0.1) #wait for serial to open
+		if arduino.isOpen():
+			print("{} connected!".format(arduino.port))
+			# définir frame comme flux video
+			frame = vs.read()
+			#frame = frame[1] if args.get("video", False) else frame
+			# fin de la boucle si pas de flux
+			if frame is None:
+				break
+			# réduction de la résolution, flou, et conversion HSV
+			frame = frame[20:450, 60:600]
+			calib = cv2.circle(frame,(84,38), 5,(0,0,255),-1)
+			calib = cv2.circle(calib,(449,36),5,(0,0,255),-1)
+			calib = cv2.circle(calib,(34,419),5,(0,0,255),-1)
+			calib = cv2.circle(calib,(517,415),5,(0,0,255),-1) 
+			imgPts = np.float32([[84,38],[449,36],[34,419],[517,415]])
+			objPoints = np.float32([[0,0],[1000,0],[0,1000],[1000,1000]])
+			matrix = cv2.getPerspectiveTransform(imgPts,objPoints)
+			frame = cv2.warpPerspective(frame,matrix,(1000,1000))
+			blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+			hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+			# génération d'un masque sur la balle
+			mask = cv2.inRange(hsv, greenLower, greenUpper)
+			mask = cv2.erode(mask, None, iterations=2)
+			mask = cv2.dilate(mask, None, iterations=2)
+			# trouver contours de la sortie du filtre et coordonnées de son centre
+			cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+			cnts = imutils.grab_contours(cnts)
+			center = None
+			# si un contour est trouvé :
+			if len(cnts) > 0:
+				c = max(cnts, key=cv2.contourArea)
+				((x, y), radius) = cv2.minEnclosingCircle(c)
+				M = cv2.moments(c)
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+				# préciser le rayon minimal pour la détection (en pixels)
+				if radius > 10:
+					# dessiner le cercle et mettre à jour la liste des dernières coordonnées (pour le trail)
+					cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 0), 2)
+					cv2.circle(frame, center, 5, (0, 0, 255), -1)			
+					#conversion en centimètres
+					x=((x/1000)*64)+1
+					y=((y/1000)*64)-0.4
+					x=round(x,1)
+					y=round(y,1)
+					cv2.putText(frame,"x,y: "+str(x)+","+str(y),(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2)
+					if (xpast>=x-0.25 and xpast<=x+0.25 and ypast>=y-0.25 and ypast<=y+0.25):
+						cv2.putText(frame,"STOP",(20,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+						counter=counter+1
+						if (counter>=15) :
+							cv2.putText(frame,"SEND",(20,90),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2)
+							#catchball(x, y)
+							while True :
+								if  arduino.inWaiting()>0: 
+									answer=arduino.readline()
+									answer=decodestr(answer)
+									print(answer)
+									arduino.flushInput()
+									if answer== 'ready' :
+										inputs="<"+str(x)+","+str(y)+">"
+										print("inputs :", inputs)
+										inputs=inputs.encode("utf-8")
+										arduino.write(inputs)
+										if answer== 'done' :
+											break
+							counter=0
+					xpast=x
+					ypast=y
+			# màj des données
+			pts.appendleft(center)
+			# fonction pour le trail rouge
+			for i in range(1, len(pts)):
+				# si pas de données, sauter
+				if pts[i - 1] is None or pts[i] is None:
+					continue
+				# calculer l'épaisseur de la ligne en fonction du temps et dessiner
+				thickness = int(np.sqrt(10 / float(i + 1)) * 2.5)
+				cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+				# afficher la sortie video traitée assemblé
+			if GPIO.input(buttonPin) == GPIO.HIGH:
+				#cv2.namedWindow("pré-calib", cv2.WND_PROP_FULLSCREEN)	
+				#cv2.setWindowProperty("pré-calib",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+				cv2.imshow("pré-calib", calib)
+			elif GPIO.input(buttonPin) == GPIO.LOW:
+				#cv2.namedWindow("Retour tracking", cv2.WND_PROP_FULLSCREEN)	
+				#cv2.setWindowProperty("Retour tracking",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+				cv2.imshow("Retour tracking", frame)
+			key = cv2.waitKey(1) & 0xFF
+			# arrêt si Q est pressé
+			if key == ord("q"):
+				break
+		# arrêt de la camera
+		vs.stop()
+		# fermer les fenetres
+		cv2.destroyAllWindows()
--- old_dev/commands_arduino.py
+++ old_dev/commands_arduino.py
@@ -1,4 +1,4 @@
-# code par Pierre Mirouze depuis un projet de pacogarcia3
+# code par Pierre Mirouze
 # FabriqExpo Exploradôme de Vitry
 # code d'envoi des coordonnées à l'arduino
 
@@ -9,20 +9,11 @@ import time
 
 class arm_controller:
 
-    def __init__(self, serialport='/dev/ttyACM1',LOW_Z=-20.5, HOVER_Z=-16.5,DROP_Z=-18.5, MID_Z=-15, HIGH_Z=-6):
+    def __init__(self, serialport='/dev/ttyACM0'):
 
         self.ser = serial.Serial(serialport, 9600)
     
         self.wait_forready()
-
-        self.LOW_Z=LOW_Z  #coordonnée la plus basse atteignable
-        self.HOVER_Z=HOVER_Z #Z pendant translation
-        self.DROP_Z=DROP_Z #Z avant de lâcher
-        self.MID_Z=MID_Z #Z intermédiaire (repos)
-        self.HIGH_Z=HIGH_Z #Z le plus haut atteignable
-
-
-        self.DELAY=0
 
 
     def decodestr(self, inputstr):   
@@ -40,7 +31,7 @@ class arm_controller:
             if str1=="ready":
                 break
 
-    def move_untildone(self, inputarr):
+    def move(self, inputarr):
 
 
         #format : <x,y,z,bool_move,bool_open,delayms,type_int> = <23,56,89,1,1,3456,3> {17}
@@ -48,7 +39,7 @@ class arm_controller:
 
         #bool_move contrôle un mouvement linéaire
         
-        inputs="<"+str(inputarr[0])+","+str(inputarr[1])+","+str(inputarr[2])+","+str(inputarr[3])+","+str(inputarr[4])+","+str(inputarr[5])+">"
+        inputs="<"+str(x)+","+str(y)">"
         inputs=inputs.encode("utf-8")
 
         self.ser.write(inputs)
@@ -57,81 +48,4 @@ class arm_controller:
             print(str1),
             if str1=="done":
                 break
-
-
-    def move_toXYZ(self, x, y, z):
-        self.move_untildone([x,y,z,1,1,self.DELAY,1])
-        
-    def move_and_pickup(self, x, y):
-
-        
-        #montée du bras
-        self.move_untildone([x,0,self.HIGH_Z,1,1,self.DELAY,1])
-        #ouvrir
-        self.move_untildone([x,0,self.HIGH_Z,1,1,self.DELAY,1])
-        #mmouvement z
-        self.move_untildone([x,0,self.HIGH_Z,1,1,self.DELAY,1])
-        #descente du bras
-        self.move_untildone([x,y,self.HOVER_Z,0,1,self.DELAY,1])
-        self.move_untildone([x,y,self.LOW_Z,1,1,self.DELAY,1])
-        #fermer
-        self.move_untildone([x,y,self.LOW_Z,1,0,self.DELAY*20,1])
-        #montée du bras
-        self.move_untildone([x,y,self.HOVER_Z,1,0,self.DELAY,1])
-        self.move_untildone([x,0,self.HIGH_Z,0,0,self.DELAY,1])
-
-
-    def transport_and_drop(self, x, y):
-
-        #monter
-        self.move_untildone([x,y,self.MID_Z,1,0,self.DELAY,1])
-        #baisser
-        self.move_untildone([x,y,self.DROP_Z,0,0,self.DELAY,1])
-        #lâcher
-        self.move_untildone([x,y,self.DROP_Z,1,1,self.DELAY*10,1])
-        #monter
-        self.move_untildone([x,0,self.HIGH_Z,0,1,self.DELAY,1])
-
-
-
-    def move_home(self):
-
-        self.move_untildone([0,0,self.HIGH_Z,1,1,self.DELAY,1])
-
-
-
-    def move_home_plane(self):
-
-        #bouger au 0 pince fermée
-        self.move_untildone([0,0,self.LOW_Z,1,0,self.DELAY,1])
-
-
-    def move_x(self,x):
-
-        self.move_untildone([x,0,self.HIGH_Z,1,0,self.DELAY,1])
-
-
-
-    def move_clearcamera(self):
-
-        self.move_untildone([40,0,self.HIGH_Z,0,1,self.DELAY,1])
-
-
-    def test_arm(self):
-
-        #home
-        self.move_home()
-
-        #libérer vue cam
-        self.move_clearcamera()
-
-        #home
-        self.move_home()
-
-        self.move_and_pickup(5, 5)
-
-        self.transport_and_drop(15,0)
-
-        #home
-        self.move_home()
 
